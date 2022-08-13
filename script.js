602c7f668adcaa731a132c509affbf4631dd202b
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-13 15:35:23 -0300

    Implementação do envio do pedido pelo WhatsApp.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let selectedItems = 0;
+let selectedDish = "";
+let selectedBeverage = "";
+let selectedDeserve = "";
+let selectedDishPrice = 0.0;
+let selectedBeveragePrice = 0.0;
+let selectedDeservePrice = 0.0;
+
 
 function deSelect(option, options) {
     const icon = option.querySelector("ion-icon");
@@ -26,6 +33,17 @@ function deSelect(option, options) {
 
     if (!exclusion) {
         selectedItems++;
+        
+        if (options === ".dishes") {
+            selectedDish = option.querySelector(".description h4").innerHTML;
+            selectedDishPrice = Number(option.querySelector(".description h5 span").innerHTML.replace(",", "."));
+        } else if (options === ".beverages") {
+            selectedBeverage = option.querySelector(".description h4").innerHTML;
+            selectedBeveragePrice = Number(option.querySelector(".description h5 span").innerHTML.replace(",", "."));
+        } else {
+            selectedDeserve = option.querySelector(".description h4").innerHTML;
+            selectedDeservePrice = Number(option.querySelector(".description h5 span").innerHTML.replace(",", "."));
+        }
     }
 
     enableButton();
@@ -40,5 +58,14 @@ function enableButton() {
     } else {
         button.classList.remove("enabled");
         button.innerHTML = "Selecione os 3 itens <br/> para fechar o pedido";
+    }
+}
+
+function checkout(button) {
+    if (button.classList.contains("enabled")) {
+        const total = (selectedDishPrice + selectedBeveragePrice + selectedDeservePrice).toFixed(2);
+        const whatsappMessage = encodeURIComponent(`Olá, gostaria de fazer o pedido:\n- Prato: ${selectedDish}\n- Bebida: ${selectedBeverage}\n- Sobremesa: ${selectedDeserve}\nTotal: R$ ${total}`);
+        
+        window.open(`https://wa.me/5521999999999?text=${whatsappMessage}`);
     }
 }
