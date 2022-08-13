2d5be23a14488d5ba012907c3ecbd43ad44352c3
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-13 01:34:24 -0300

    Implementação do botão Fechar Pedido.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let selectedItems = 0;
+
 function deSelect(option, options) {
     const icon = option.querySelector("ion-icon");
     const selected = document.querySelector(options + " .selected");
+    let exclusion = false;
 
     if (selected) {   
         const iconSelected = selected.querySelector("ion-icon");
@@ -12,9 +15,30 @@ function deSelect(option, options) {
         if (selected === option) {
             option.classList.toggle("selected");
             icon.classList.toggle("hidden");
+            exclusion = true;
         }
+
+        selectedItems--;
     }
     
     option.classList.toggle("selected");
     icon.classList.toggle("hidden");
+
+    if (!exclusion) {
+        selectedItems++;
+    }
+
+    enableButton();
+}
+
+function enableButton() {
+    const button = document.querySelector("button");
+
+    if (selectedItems === 3) {
+        button.classList.add("enabled");
+        button.innerHTML = "Fechar pedido";
+    } else {
+        button.classList.remove("enabled");
+        button.innerHTML = "Selecione os 3 itens <br/> para fechar o pedido";
+    }
 }
